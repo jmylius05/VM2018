a18c99c79df86391e59b82016f1c6e269f7adcb6
Jessica Mylius <mylius@hawaii.edu>
2018-11-05 15:56:28 -0800

    Initial Commit

---
+++ VisitMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VisitMe
+//
+//  Created by Jessica Mylius on 11/5/18.
+//  Copyright © 2018 Jess Mundo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
